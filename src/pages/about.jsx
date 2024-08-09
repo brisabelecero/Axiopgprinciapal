cbf384cbad1091445f4382cf10f2f8e2e55064c8
Abimael <abimaeljuarezc@gmail.com>
2024-08-09 15:09:28 -0600

    v0.0.5

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -6,18 +6,23 @@ const about = () => {
     <div>
       <BannerComp title={"¿Quíenes Somos?"} image={"../assets/images/quienes-1.jpg"} />
       <section className='mission'>
-        <h2>Misión</h2>
-        <p>Nuestra misión es impulsar una agenda de ética y valores en el gobierno, la empresa privada 
-          y la educación que coadyuve a prevenir el fenómeno de la corrupción, la injustica, la delincuencia, 
-          la violencia y todos aquellos males que lastiman a la sociedad.</p>
+        <div className="txt-container">
+          <h2>Misión</h2>
+          <p>Nuestra misión es impulsar una agenda de ética y valores en el gobierno, la empresa privada 
+           y la educación que coadyuve a prevenir el fenómeno de la corrupción, la injustica, la delincuencia, 
+           la violencia y todos aquellos males que lastiman a la sociedad.</p>
+        </div>
       </section>
       <section className='vision'>
-        <h2>Visión</h2>
-        <p>Para 2030 lograremos implementar nuestro ecosistema Axio, nuestra metodología de gestión basada en 
-          la integridad y nuestros diagnósticos de cultura de integridad en más de cinco países de Latinoamérica</p>
+        <div className="txt-container">
+          <h2>Visión</h2>
+          <p>Para 2030 lograremos implementar nuestro ecosistema Axio, nuestra metodología de gestión basada en 
+           la integridad y nuestros diagnósticos de cultura de integridad en más de cinco países de Latinoamérica</p>
+        </div>
       </section>
       <section className='values'>
         <h2>Nuestros Valores</h2>
+        <div className="cards-container"></div>
       </section>
     </div>
   )
