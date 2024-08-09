59165fd70b42d4e85f0b85336f3b894e25de150b
Abimael <abimaeljuarezc@gmail.com>
2024-08-09 14:33:34 -0600

    v0.0.3

--- src/pages/certifications.jsx
+++ src/pages/certifications.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import BannerComp from '../Components/BannerComp/BannerComp'
 
 const certifications = () => {
   return (
     <div>
-      
+      <BannerComp title={"Certificaciones"} image={"../assets/images/certif-1.png"} />
     </div>
   )
 }
