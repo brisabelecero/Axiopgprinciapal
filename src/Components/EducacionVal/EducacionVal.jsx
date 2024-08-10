98fdcc3d00775a88508e360186716d4da297cd34
Abimael <abimaeljuarezc@gmail.com>
2024-08-09 19:21:49 -0600

    v0.0.6

---
+++ src/Components/EducacionVal/EducacionVal.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import './EducacionVal.css'
+
+const EducacionVal = () => {
+  return (
+    <div>
+        <section className="certif-section-a">
+            <p>En la educación descansan las esperanzas de un mundo mejor. 
+             Un mundo libre del cáncer de la corrupción, del azote de la violencia 
+             y de la tragedia de las desigualdades sociales.</p>
+        </section>
+        <section className="certif-section-b">
+            <p>Múltiples y variados diagnósticos develan que los esfuerzos de los sistemas educativos 
+                no están siendo suficientes para hacer frente a los complejos fenómenos sociales que 
+                lastiman a nuestra sociedad, pero principalmente la evidente realidad nos demanda una 
+                transformación de la educación y una revisión de los paradigmas que sostienen los modelos 
+                educativos actuales. En otras palabras, <span class="certif-distinction">no existe mejor evaluación de la educación que la 
+                cruda realidad social y no tenemos buenos resultados</span>. Altos índices de</p>
+            <ul className="list-certif"> 
+                <li><p>Violencia Escolar</p></li>
+                <li><p>Embarazos Prematuros</p></li>
+                <li><p>Adicciones</p></li>
+                <li><p>Suicidios</p></li>
+                <li><p>Deserción Escolar</p></li>
+                <li><p>Bajo Rendimiento</p></li>
+            </ul>
+            <p>encabezan la lista de prioridades de la mayoría de las comunidades educativas.</p>
+        </section>
+        <section className="certif-section-c">
+            <p>Las visiones pragmáticas y utilitaristas de la educación no han dado 
+                respuesta a estos desafíos y muchas de las políticas educativas apenas rozan 
+                la superficialidad del fenómeno, dejando al descubierto un gran vacío educativo, 
+                un vacío que solo puede llenarse con los valores que son inherentes y universales 
+                a la naturaleza humana. Valores que nos permiten vivir en armonía, en prosperidad y 
+                en la plenitud de nuestras potencialidades. Es necesario reconocer que los valores 
+                universales son la respuesta a los desafíos sociales de todos los tiempos</p>
+            <p>Es fundamental también reconocer que a la importancia de la formación axiológica 
+                se ha sumado un sentido de urgencia y de acción inmediata. 
+                <span class="certif-distinction-blue"> ¡No hay tiempo que perder!</span></p>
+        </section>
+        <section className="certif-section-d">
+            <p>El desafío no es menor. Más allá de reconocer que es necesario fortalecer la formación 
+                en valores en los hogares y escuelas, se necesitan acciones coordinadas que tengan un 
+                sólido fundamento científico alejado de cualquier ideología religiosa o política. Se requiere 
+                certeza en el rumbo y la convicción de mantenerse en la ruta trazada.</p>
+        </section>
+        <section className="certif-section-e">
+            <p>La certificación <span class="certif-distinction-blue"> “Educación con valores” </span> busca ser un motor que contribuya a la suma de voluntades 
+                y a la coordinación de las acciones bajo la implementación de una metodología de gestión escolar 
+                que logra el esfuerzo coordinado de padres, directivos y docentes para que en la escuela se hagan 
+                vida los valores en la cotidianidad.</p>
+            <p><span class="certif-distinction"> ¡Certifica a tu institución educativa! </span> </p>
+        </section>
+    </div>
+  )
+}
+
+export default EducacionVal
--- src/pages/certifications.jsx
+++ src/pages/certifications.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import BannerComp from '../Components/BannerComp/BannerComp'
+import EducacionVal from '../Components/EducacionVal/EducacionVal'
 
 const certifications = () => {
+  const [activeSection, setActiveSection] = useState('educacionConValores')
+
+  const handleButtonClick = (section) => {
+    setActiveSection(section)
+  }
+
   return (
     <div>
       <BannerComp title={"Certificaciones"} image={"../assets/images/certif-1.png"} />
+      <div className="btn-container">
+        <button className="btn-certif" onClick={() => handleButtonClick('educacionConValores')}>Educación con Valores</button>
+        <button className="btn-certif" onClick={() => handleButtonClick('gobiernoConValores')}>Gobierno con Valores</button>
+        <button className="btn-certif" onClick={() => handleButtonClick('gestionPorValores')}>Gestión por Valores</button>
+        <button className="btn-certif" onClick={() => handleButtonClick('certificacionIndividual')}>Certificación Individual</button>
+      </div>
+      <div className="section-container">
+        {activeSection === 'educacionConValores' && <EducacionVal />}
+      </div>
     </div>
   )
 }
