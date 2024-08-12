6fc192210f9205301592c2e9e7438869de85dc50
Abimael <abimaeljuarezc@gmail.com>
2024-08-12 04:10:02 -0600

    v0.1.0

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,23 +1,27 @@
 import React from 'react'
 import BannerComp from '../Components/BannerComp/BannerComp'
+import ImgMission from '../assets/images/ImgMision.jpg'
+import ImgVision from '../assets/images/ImgVision.jpg'
 
 const about = () => {
   return (
     <div>
       <BannerComp title={"¿Quíenes Somos?"} image={"../src/assets/images/quienes-1.jpg"} />
-      <section className='mission'>
-        <div className="txt-container">
+      <section className='mission'>        
+        <div className="txt-container-a">
           <h2>Misión</h2>
           <p>Nuestra misión es impulsar una agenda de ética y valores en el gobierno, la empresa privada 
            y la educación que coadyuve a prevenir el fenómeno de la corrupción, la injustica, la delincuencia, 
            la violencia y todos aquellos males que lastiman a la sociedad.</p>
         </div>
+        <img src={ImgMission} alt="" />
       </section>
       <section className='vision'>
-        <div className="txt-container">
+        <img src={ImgVision} alt="" />
+        <div className="txt-container-b">
           <h2>Visión</h2>
           <p>Para 2030 lograremos implementar nuestro ecosistema Axio, nuestra metodología de gestión basada en 
-           la integridad y nuestros diagnósticos de cultura de integridad en más de cinco países de Latinoamérica</p>
+           la integridad y nuestros diagnósticos de cultura de integridad en más de cinco países de Latinoamérica.</p>
         </div>
       </section>
       <section className='values'>
