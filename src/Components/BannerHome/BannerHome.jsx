6c6ae382f39f85ec32c1bcc55a88d2c53c2630d2
Abimael <abimaeljuarezc@gmail.com>
2024-08-17 12:38:28 -0600

    v0.1.4.5.2

--- src/Components/BannerHome/BannerHome.jsx
+++ src/Components/BannerHome/BannerHome.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './BannerHome.css'
 import { LottieAnim } from '../LottieComp/LottieAnim'
 import animacion1 from '../LottieComp/animations/animation1.json'
 
 const BannerHome = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+  const [anchoAnim, setAncho] = useState('612.2px')
+  const [altoAnim, setAlto] = useState('407.72px')
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth)
+      if (window.innerWidth > 1024){
+        setAncho('612.2px')
+        setAlto('407.72px')
+      }else if (window.innerWidth >= 768){
+        setAncho('400px')
+        setAlto('300px')
+      }else{
+        setAncho('120px')
+        setAlto('150px')
+      }
+    }
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
 
   return (
     <section className='banner-home'>
@@ -13,7 +34,7 @@ const BannerHome = () => {
         <button className="banner-home-btn">Descubrir</button>
       </div>
       <div>
-        <LottieAnim animacion={animacion1} ancho={"612.2px"} alto={"407.72px"} />
+        <LottieAnim animacion={animacion1} ancho={anchoAnim} alto={altoAnim} />
       </div>
     </section>
   )
