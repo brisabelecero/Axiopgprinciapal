74e003d87c0d1ea6744ba6bfd646df9c539c8500
Abimael <abimaeljuarezc@gmail.com>
2024-08-22 21:27:02 -0600

    v0.1.4.6.4

--- src/Components/BannerDigital/BannerDigital.jsx
+++ src/Components/BannerDigital/BannerDigital.jsx
@@ -1,17 +1,31 @@
 import React from 'react'
 import '../BannerDigital/BannerDigital.css'
 import googlePlayButton from '../../assets/images/ImgPlay.png'
+import appStoreButton from '../../assets/images/ImgStore.png'
+import ImgPhone2 from '../../assets/images/phone2.png'
+import ImgPhone1 from '../../assets/images/phone1.png'
+
 
 const BannerDigital = ({title, image}) => {
     const googlePlayLink = 'https://play.google.com/store/apps/details?id=com.iwebsapp.axioparents&hl=es_MX'
 
   return (
     <div className="banner-digital" style={{ backgroundImage: `url(${image})` }}>
-      <h1 className="banner-digital-title">{title}</h1>
-      <div className="button-container">
-        <a href={googlePlayLink} target="_blank" rel="noopener noreferrer">
-            <img src={googlePlayButton} alt="Disponible en Google Play" />
-        </a>
+      <div className="banner-digital-container-a">
+        <h1 className="banner-digital-title">{title}</h1>
+        <p>Descarga nuestra app ahora y conoce la plataforma de Axio.</p>
+        <div className="button-container">
+          <a href={googlePlayLink} target="_blank" rel="noopener noreferrer">
+              <img src={googlePlayButton} alt="Disponible en Google Play" className="btn-store"/>
+          </a>
+          <a href="" target="_blank" rel="noopener noreferrer">
+            <img src={appStoreButton} alt="Disponible en App Store" className="btn-store"/>
+          </a>
+        </div>
+      </div>
+      <div className="img-phone-container">
+        <img src={ImgPhone2} alt="Img Phone 2" className="img-phone-2" />
+        <img src={ImgPhone1} alt="Img Phone 1" className="img-phone-1" />
       </div>
     </div>
   )
