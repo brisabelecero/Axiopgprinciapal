48814664ddd56b5f9480eff7b1727416889e57ce
Abimael <abimaeljuarezc@gmail.com>
2024-08-24 18:57:27 -0600

    v0.1.5.3

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,29 @@ import React from 'react'
 import HeaderComp from './Components/Header/HeaderComp'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-import Home from '../src/pages/home'
-import About from '../src/pages/about'
-import Certifications from '../src/pages/certifications'
-import Digital from '../src/pages/digital'
-import Services from '../src/pages/services'
-import Contact from '../src/pages/contact'
+import Home from './pages/home'
+import About from './pages/about'
+import Certifications from './pages/certifications'
+import Digital from './pages/digital'
+import Services from './pages/services'
+import Contact from './pages/contact'
 
 function App() {
+  
   return (
-    <BrowserRouter>
-      <HeaderComp />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/certifications' element={<Certifications />} />
-        <Route path='/digital' element={<Digital />} />
-        <Route path='/services' element={<Services />} />
-        <Route path='/contact' element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <HeaderComp />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/certifications' element={<Certifications />} />
+          <Route path='/digital' element={<Digital />} />
+          <Route path='/services' element={<Services />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
